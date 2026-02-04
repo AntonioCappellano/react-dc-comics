7882cd3a53bc8d3bd2d212d81a15daecc88c06e4
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-04 17:43:25 +0100

    add second and third section footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,60 @@ export default function Footer() {
                 </figure>
             </div>
         </section>
-        <section></section>
-        <section></section>
+        <section>
+            <div id="container-footer" className="container">
+                <div className="row row-cols-auto gap-2 mt-4 text-center text-white">
+                    <div className="col">
+                        <h5>DC COMICS</h5>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <p>characters</p>
+                        <h5>SHOPS</h5>
+                        <p>Shop</p>
+                        <p>Shop</p>
+                    </div>
+                    <div className="col">
+                        <h5>DC</h5>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+                        <p>Terms</p>
+
+                    </div>
+                    <div className="col">
+                        <h5>SITES</h5>
+                        <p>DC</p>
+                        <p>DC</p>
+                        <p>DC</p>
+                        <p>DC</p>
+                        <p>DC</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="third-section">
+            <div className="container d-flex justify-content-between align-items-center text-white py-4">
+                <button type="button" className="btn btn-outline-primary text-white rounded-0">SIGN-UP NOW!</button>
+                <div className="d-flex align-items-center gap-3">
+                <span className="me-3 text-primary">FOLLOW US</span>
+                <img src="src/assets/img/footer-facebook.png"></img>
+                <img src="src/assets/img/footer-twitter.png"></img>
+                <img src="src/assets/img/footer-youtube.png"></img>
+                <img src="src/assets/img/footer-pinterest.png"></img>
+                <img src="src/assets/img/footer-periscope.png"></img>
+                </div>
+            </div>
+        </section>
         </footer>
 }
