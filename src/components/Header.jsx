5a691cf897d2ac08675464af6f12675edcfed9e4
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-04 15:37:41 +0100

    end header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,28 @@
 export default function Header() {
-    return <header>Io sono l'Header</header>
+    return <header>
+        <div className="container mt-2 d-flex align-items-center justify-content-between">
+         <figure>
+                <img src="src\assets\img\dc-logo.png" alt="dc-logo"></img>
+         </figure> 
+
+        <ul className="list-unstyled mb-0">
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> CHARACTERS</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> COMICS</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> MOVIES</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> TV</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> GAMES</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> COLLECTIBLES</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> VIDEOS</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> FANS</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> NEWS</a> </li>
+            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> SHOP</a> </li>
+         </ul>
+        </div>
+    </header>
 }
+
+
+
+ 
+
+
