bfa23da42c8ca3527c50061645a26f68f95d4548
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-04 16:29:31 +0100

    add first section footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,30 @@
 export default function Footer() {
-    return <footer>Io sono il Footer</footer>
+    return <footer>
+        <section id="buys-section">
+            <div className="container d-flex  align-items-center gap-4">
+                <figure className="m-5 d-flex align-items-center gap-2">
+                    <img className="buys-img" src="src\assets\img\buy-comics-digital-comics.png"></img>
+                    <figcaption className="text-white">DIGITAL COMICS</figcaption>
+                </figure>
+                <figure className="mt-3 d-flex align-items-center gap-2">
+                    <img className="buys-img" src="src\assets\img\buy-comics-merchandise.png"></img>
+                    <figcaption className="text-white">DC MERCHANDISE</figcaption>
+                </figure>
+                <figure className="mt-3 d-flex align-items-center gap-2">
+                    <img className="buys-img" src="src\assets\img\buy-comics-subscriptions.png"></img>
+                    <figcaption className="text-white">SUBSCRIPTION</figcaption>
+                </figure>
+                <figure className="mt-3 d-flex align-items-center gap-2">
+                    <img className="buys-img" src="src\assets\img\buy-comics-shop-locator.png"></img>
+                    <figcaption className="text-white">COMIC SHOP LOCATOR</figcaption>
+                </figure>
+                <figure className="mt-3 d-flex align-items-center gap-2">
+                    <img className="buys-img" src="src\assets\img\buy-dc-power-visa.svg"></img>
+                    <figcaption className="text-white">DC POWER VISA</figcaption>
+                </figure>
+            </div>
+        </section>
+        <section></section>
+        <section></section>
+        </footer>
 }
