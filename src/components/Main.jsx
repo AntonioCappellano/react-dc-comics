d99cc72b5afea01aa75f073d4d0fa99399139b9c
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-04 15:47:29 +0100

    add main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,9 @@
 export default function Main() {
-    return <main>Io sono il Main</main>
+    return <main>
+        <section id="main-section">
+        <div className="container py-5 text-white"> 
+            <h1>Content goes here</h1>
+            </div> 
+            </section>
+            </main>
 }
