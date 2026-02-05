18c9c4925cfa57e6f78acf0a3725b941a4f3fa0b
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-05 14:27:19 +0100

    adjust header with correct date structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,49 @@
 export default function Header() {
+    
+    const navBarLinks = [
+        {
+            url: "#",
+            text: "CHARACTERS"
+        },
+        {
+            url: "#",
+            text: "COMICS"
+        },
+        {
+            url: "#",
+            text: "MOVIES"
+        },
+        {
+            url: "#",
+            text: "TV"
+        },
+        {
+            url: "#",
+            text: "GAMES"
+        },
+        {
+            url: "#",
+            text: "COLLECTIBLES"
+        },
+        {
+            url: "#",
+            text: "VIDEOS"
+        },
+        {
+            url: "#",
+            text: "FANS"
+        },
+        {
+            url: "#",
+            text: "NEWS"
+        },
+        {
+            url: "#",
+            text: "SHOP"
+        },
+       
+    ] 
+
     return <header>
         <div className="container mt-2 d-flex align-items-center justify-content-between">
          <figure>
@@ -6,16 +51,10 @@ export default function Header() {
          </figure> 
 
         <ul className="list-unstyled mb-0">
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> CHARACTERS</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> COMICS</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> MOVIES</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> TV</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> GAMES</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> COLLECTIBLES</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> VIDEOS</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> FANS</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> NEWS</a> </li>
-            <li className="d-inline-block mx-3"> <a href="#" className="pe-none text-muted text-decoration-none"> SHOP</a> </li>
+            {navBarLinks.map((links, index) =>(
+                <li key={index} className="d-inline-block mx-3"> <a href={links.url} > {links.text}</a> </li>
+            ))}
+            
          </ul>
         </div>
     </header>
